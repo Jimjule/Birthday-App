89fbedd3381dea01e2186e720941d12f78564904
Jimjule <roadknightj@gmail.com>
2019-02-22 11:07:19 +0000

    Refactored feature test to include all data

---
+++ process.md
@@ -0,0 +1,8 @@
+# Birthday App
+
+
+# Process
+
+RED - Created app, config, and index view. Wrote feature test expecting page to greet user after submitting their name in a field.
+
+ 
--- spec/features/birthday_spec.rb
+++ spec/features/birthday_spec.rb
@@ -1,17 +1,11 @@
-feature 'enter name' do
-  scenario 'user can submit name and have it appear on screen' do
-    visit('/')
-    fill_in :name, with: 'Joey Jo-jo Junior'
-    click_button 'Submit'
-    expect(page).to have_content 'Hello Joey Jo-jo Junior'
-  end
-
+feature 'enter details' do
   scenario 'user can submit birthday and have it appear on screen' do
     visit('/')
     fill_in :name, with: 'Bort'
     fill_in :day, with: 5
+    select('March', :from => :month)
     click_button 'Submit'
-    expect(page).to have_content 'Hello Bort, 5'
+    expect(page).to have_content 'Hello Bort, 5, 03'
   end
 
 end
